6b535b29e36de25d787db67b9e1f256d217026f4
Nikita <dragonnp@ya.ru>
2021-12-07 21:24:39 +0300

    Добавлено логгирование

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 import random
-
+import logging
 import telegram
 import texts
 from telegram import Update
@@ -12,11 +12,15 @@ from telegram.ext import (
     CallbackContext,
 )
 
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger('main.py')
+
 
 def random_nums():
     res = ''
     for i in range(13):
-        res += str(random.randint(0,9))
+        res += str(random.randint(0, 9))
     return res
 
 
@@ -26,12 +30,18 @@ def start_hello(update: Update, context: CallbackContext):
 
 def start(update: Update, context: CallbackContext):
     if len(context.args) == 1:
+        logger.info(f'{update.message.from_user.full_name}({update.message.from_user.link}) '
+                    f'захотел(а) написать ему: {context.args[0]}')
+
         context.user_data['Valentine'] = True
         context.user_data['Recipient'] = context.args[0]
         context.user_data['SenderLink'] = update.message.from_user.link
 
         update.message.reply_text(texts.start(), parse_mode=telegram.ParseMode.MARKDOWN)
     else:
+        logger.info(f'{update.message.from_user.full_name}({update.message.from_user.link}) '
+                    f'ввел(а) /start без айдишника')
+
         clear_vars(context)
         start_hello(update, context)
 
@@ -40,16 +50,19 @@ def any_msg(update: Update, context: CallbackContext):
     if 'Valentine' in context.user_data and context.user_data['Valentine']:
         recipient = context.user_data['Recipient']
         link = context.user_data['SenderLink']
-
     else:
         recipient = update.effective_user.id
         link = update.message.from_user.link
+    logger.info(f'{update.message.from_user.full_name}({link}) '
+                f'отправил(а) сообщение "{update.message.text}" ему: {recipient}')
 
-    context.bot.sendMessage(recipient, f'''У вас новое анонимное признание ([#]({link}){random_nums()}):\n\n*{update.message.text}*''', parse_mode=telegram.ParseMode.MARKDOWN, disable_web_page_preview=True)
+    context.bot.sendMessage(recipient,
+                            f'У вас новое анонимное признание ([#]({link}){random_nums()}):\n\n*{update.message.text}*',
+                            parse_mode=telegram.ParseMode.MARKDOWN,
+                            disable_web_page_preview=True)
     update.message.reply_text(texts.done().format(update.effective_user.id), parse_mode=telegram.ParseMode.MARKDOWN)
 
     clear_vars(context)
-    print(f'Отправлена валентинка от {link}, кому: {recipient}')
 
 
 def clear_vars(context: CallbackContext):
